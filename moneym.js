969d87cf237ef42481255fc35a63fb200b444563
Vitaliy Teremasov <teremasov.vitalik@gmail.com>
2015-09-14 19:36:54 +0600

    Installed accounts:password and accounts:bootstrap Used iron-routs. created simple auth

--- moneym.js
+++ moneym.js
@@ -1,18 +1,37 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  //Session.setDefault('counter', 0);
+  //
+  //Template.hello.helpers({
+  //  counter: function () {
+  //    return Session.get('counter');
+  //  }
+  //});
+  //
+  //Template.hello.events({
+  //  'click button': function () {
+  //    // increment the counter when button is clicked
+  //    Session.set('counter', Session.get('counter') + 1);
+  //  }
+  //});
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Router.configure({
+    notFoundTemplate: '404'
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Router.route('/', function () {
+    // render the Home template with a custom data context
+    this.render('Home');
+  });
+
+  Router.route('/login', function () {
+    // render the Home template with a custom data context
+    this.render('login');
+  });
+
+  Router.route('/signUp', function () {
+    // render the Home template with a custom data context
+    this.render('signUp');
   });
 }
 
