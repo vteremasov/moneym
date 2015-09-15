5d80c77805eec9b97b9e1cd6cef0ac18cb1893a1
Vitaliy Teremasov <teremasov.vitalik@gmail.com>
2015-09-15 17:07:12 +0600

    Added new packages moneym.html devided for templates Configured login package

--- moneym.js
+++ moneym.js
@@ -1,38 +1,91 @@
+'use strict';
+
+Router.plugin('ensureSignedIn', {
+  except: ['home', 'signIn', 'signUp', 'atForgotPassword']
+});
+
+Router.configure({
+  layoutTemplate: 'main',
+  //notFoundTemplate: '404',
+  yieldTemplates: {
+    nav: {to: 'nav'}
+    //  myFooter: {to: 'footer'}
+  }
+});
+
+AccountsTemplates.configure({
+  defaultLayout: 'main',
+  confirmPassword: true,
+  enablePasswordChange: true,
+  showPlaceholders: true,
+  showForgotPasswordLink: true,
+  lowercaseUsername: false,
+  focusFirstInput: true,
+  negativeValidation: true,
+  positiveValidation: true,
+  positiveFeedback: true,
+  showValidating: true
+});
+
+var pwd = AccountsTemplates.removeField('password');
+AccountsTemplates.removeField('email');
+AccountsTemplates.addFields([
+  {
+    _id: "username",
+    type: "text",
+    displayName: "username",
+    required: true,
+    minLength: 3
+  },
+  {
+    _id: 'email',
+    type: 'email',
+    required: true,
+    displayName: "email",
+    re: /.+@(.+){2,}\.(.+){2,}/,
+    errStr: 'Invalid email'
+  },
+  pwd
+]);
+
+AccountsTemplates.configureRoute('signIn', {
+  name: 'signin',
+  path: '/login',
+  template: 'fullPageAtForm',
+  layoutTemplate: 'main',
+  redirect: '/wallets'
+});
+
+AccountsTemplates.configureRoute('signUp', {
+  name: 'signUp',
+  path: '/sign-up',
+  template: 'fullPageAtForm',
+  layoutTemplate: 'main',
+  redirect: '/wallets'
+});
+
+Router.route('/', function () {
+  this.layout('main');
+  this.render("nav", {to: "nav"});
+  this.render('home');
+}, {
+  name: 'home'
+});
+
+
+Router.route('/wallets', function () {
+  this.layout('main');
+  this.render("nav", {to: "nav"});
+  this.render('wallets');
+}, {
+  name: 'wallets'
+});
+
+var wallets = new Mongo.Collection("wallets");
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  //Session.setDefault('counter', 0);
-  //
-  //Template.hello.helpers({
-  //  counter: function () {
-  //    return Session.get('counter');
-  //  }
-  //});
-  //
-  //Template.hello.events({
-  //  'click button': function () {
-  //    // increment the counter when button is clicked
-  //    Session.set('counter', Session.get('counter') + 1);
-  //  }
-  //});
 
-  Router.configure({
-    notFoundTemplate: '404'
-  });
-
-  Router.route('/', function () {
-    // render the Home template with a custom data context
-    this.render('Home');
-  });
-
-  Router.route('/login', function () {
-    // render the Home template with a custom data context
-    this.render('login');
-  });
-
-  Router.route('/signUp', function () {
-    // render the Home template with a custom data context
-    this.render('signUp');
-  });
 }
 
 if (Meteor.isServer) {
